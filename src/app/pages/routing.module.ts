557bd5d04808fc0a80100fe2c2fcc23b71f1e657
Abrahao Neto <abrahao-neto@outlook.com>
2022-06-30 00:08:14 -0300

    Iniciando o projeto

--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//Components
+import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)
+    component: HomeComponent
   }
 ];
 
@@ -12,4 +15,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class RoutingModule { }
